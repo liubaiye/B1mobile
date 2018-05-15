eea376b2e4e32e441f63b308a2af9e991f9a9416
iliubin <704251611@qq.com>
2018-05-15 08:37:42 +0800

    封装了fetch请求，登陆权限

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,29 +1,26 @@
 <template>
   <div class="login-page before">
-      <div class="login-wrap">
-        <div class="login-head">
-            <img src="../assets/images/8223856_03.png" width="75"/>
-        </div>
-        <group class="group">
-          <x-input  v-model="form.value1"  placeholder="请输入用户名">
-            <span slot="label" class="el-icon-fa-user size20"></span>
-          </x-input>
-          <x-input  v-model="form.passWord" type='password' placeholder="请输入密码">
-            <span slot="label" class="el-icon-fa-lock size20"></span>
-          </x-input>
-          <selector ref="defaultValueRef" direction="rtl" :options="list" v-model="defaultLangage"></selector>
-         </group>
-       
-        <div style="padding:15px;">
-          <x-button type="primary" @click.native="login">登　录</x-button>
-        </div>
-        <div class="find-pass text-right">
-          <router-link :to="{path:'/FindPass'}"
-          >忘记密码？立即找回</router-link>
-        </div>
- 
-       
+    <div class="login-wrap">
+      <div class="login-head">
+        <img src="../assets/images/8223856_03.png" width="75" />
       </div>
+      <group class="group">
+        <x-input v-model="form.accountName" placeholder="请输入用户名" required>
+          <span slot="label" class="el-icon-fa-user size20"></span>
+        </x-input>
+        <x-input v-model="form.password" type='password' placeholder="请输入密码" required>
+          <span slot="label" class="el-icon-fa-lock size20"></span>
+        </x-input>
+        <selector ref="defaultValueRef" direction="rtl" :options="list" v-model="defaultLangage"></selector>
+      </group>
+      <div style="padding:15px;">
+        <x-button type="primary" @click.native="login">登　录</x-button>
+      </div>
+      <div class="find-pass text-right">
+        <router-link :to="{path:'/FindPass'}">忘记密码？立即找回</router-link>
+      </div>
+      <toast v-model="loginState.showClose" type="warn">{{loginState.Message}}</toast>
+    </div>
   </div>
 </template>
 <i18n>
@@ -31,7 +28,9 @@
 </i18n>
 <script>
   import Vue from "vue";
+  import fetchs from '../fetch/fetch';
   import {
+    Toast,
     Selector,
     XInput,
     Cell,
@@ -46,11 +45,12 @@
     XAddress,
     ChinaAddressV4Data,
     Swiper,
-    XButton 
+    XButton
   } from "vux";
   export default {
     name: "login-page",
     components: {
+      Toast,
       Selector,
       XInput,
       XButton,
@@ -69,40 +69,83 @@
     },
     data() {
       return {
-        defaultLangage:'ch',
-         list: [{key: 'ch', value: '中文'}, {key: 'en', value: 'English'}],
-         form:{
-           value1:'',
-           passWord:''
-         }
+        defaultLangage: 'ch',
+        list: [{
+          key: 'ch',
+          value: '中文'
+        }, {
+          key: 'en',
+          value: 'English'
+        }],
+        form: {
+          accountName: '',
+          password: ''
+        },
+        loginState: {
+          showClose: false,
+          Message: ''
+        }
       }
     },
     methods: {
-      login(){
-        this.$router.replace('/Home');
+      login() {
+        let self = this 
+        fetchs.fetch({
+          data:self.form,
+          url:'/api/user/auth/login/0',
+          success:(res)=>{
+            this.$router.replace('/Home');
+          },
+          erro:(res)=>{
+            self.loginState = {
+              showClose: true,
+               Message: res.result.errorMessage
+            }
+          }
+        })
       }
-    }
+      // ajax({
+      //   url:'/api/user/auth/login/0',
+      //   type: 'POST',
+      //   data:{
+      //     accountName:self.form.accountName,
+      //     password:self.form.password
+      //   },
+      //   success:(res)=>{
+      //     debugger
+      //   },
+      //   error:(res)=>{
+      //     
+      //   }
+      // })
+      // 
+  
+  },
+  mounted() {
+
+  }
   };
 
 </script>
 <style scoped>
-.size20{
-  font-size: 20px!important;
-  padding-right: 10px;
-      /* color: #949494; */
-  color: #ff7800;
-}
- .login-page{
-   width: 100%;
-   height: 100%;
-   /* background-image: url(../assets/images/login_1.png);
+  .size20 {
+    font-size: 20px !important;
+    padding-right: 10px;
+    /* color: #949494; */
+    color: #ff7800;
+  }
+
+  .login-page {
+    width: 100%;
+    height: 100%;
+    /* background-image: url(../assets/images/login_1.png);
    background-repeat: no-repeat;
    background-size: cover;*/
-   text-align: center; 
- }
+    text-align: center;
+  }
 
- .login-wrap{
-       width: 100%;
+  .login-wrap {
+    width: 100%;
     display: inline-block;
     background-color: #fff;
     vertical-align: middle;
@@ -110,5 +153,6 @@
 
     -webkit-box-sizing: border-box;
     box-sizing: border-box;
-}
+  }
+
 </style>
--- src/util/util.js
+++ src/util/util.js
@@ -2,18 +2,18 @@ import rc4 from './rc4'
 import config from './config'
 
 export default {
-  buildUrl(url) {
-    // return 'http://192.168.1.128:9002/'+url
-    // if (window.DEBUG) {
-    //   // return 'http://192.168.1.128:9002/'+url
-     return 'http://localhost:25425/' + url
-    // }
-    
-  },
-  encrypt(text) {
-    return rc4(text, config.encryptKey)
-  },
-  decrypt(text) {
-    return rc4(text, config.encryptKey)
-  }
+    buildUrl(url) {
+        // return 'http://192.168.1.128:9002/'+url
+        // if (window.DEBUG) {
+        //   // return 'http://192.168.1.128:9002/'+url
+        return 'http://118.178.190.171:8080/accchn-controller' + url
+            // }
+
+    },
+    encrypt(text) {
+        return rc4(text, config.encryptKey)
+    },
+    decrypt(text) {
+        return rc4(text, config.encryptKey)
+    }
 }
